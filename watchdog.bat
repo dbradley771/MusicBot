62bd5a44a4aeecb9c8226c45b088073d787aabb9
David Bradley <david@dbradley771.com>
2018-04-30 02:32:58 -0500

    Add watchdog

---
+++ watchdog.bat
@@ -0,0 +1,20 @@
+@echo off
+
+cls
+
+echo Protecting MusicBot from crashes...
+
+echo If you want to close MusicBot and this script, close the MusicBot window and type Y depending on your language followed by Enter.
+
+title MusicBot Watchdog
+
+:MusicBot
+
+echo (%time%) MusicBot started.
+
+start /wait run.bat
+
+echo (%time%) WARNING: MusicBot closed or crashed, restarting.
+
+goto MusicBot
+
